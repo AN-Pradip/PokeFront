4c3b18d9d154399158ba284894e267a722f704ec
AN-Pradip <ppradip.an@gmail.com>
2023-05-14 16:43:24 +0200

    adding conditional rendering of gender button

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import RightPanel from "./layout/rightpanel";
 import {PokemonContext} from "./PokemonContext"
 
 function App() {
-  const [pokemonInfo, setPokemonInfo] = useState({name: "pikachu"})
+  const [pokemonInfo, setPokemonInfo] = useState({name: "pikachu",id: 25})
+  const [pokemonGender, setPokemonGender] = useState(4)
 
   const pokemonNameRef = useRef(undefined);
   useEffect(() => {
     //Search each time pokemon name is changed
     SearchPokemon(pokemonInfo)
+    SearchPokemonGender(pokemonInfo)
     }, [pokemonInfo.name]);
 
   //prevent refresh & set searched pokemon name to lower case
@@ -18,6 +20,7 @@ function App() {
     setPokemonInfo({name: pokemonNameRef.current.value.toLowerCase()})
   }
 
+  //fetch pokemon Info
   async function SearchPokemon(pokemonInfo){
     await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInfo.name}`, {
       method: "GET"
@@ -32,8 +35,25 @@ function App() {
     })
   }
 
+  //fetch pokemon gender
+  async function SearchPokemonGender(pokemonInfo){
+    console.log(pokemonInfo)
+    await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonInfo.name}`, {
+        method: "GET"
+    })
+    .then(response => response.json())
+    .then((data) => {
+        //console.log(data.gender_rate)
+        setPokemonGender(data.gender_rate)
+        //console.log(pokemonGender)
+    })
+    .catch(function (error) {
+        console.log("ERROR: pokemon gender not found. " + error.status)
+    })
+}
+
   return (
-    <PokemonContext.Provider value={{pokemonInfo, setPokemonInfo}}>
+    <PokemonContext.Provider value={{pokemonInfo, setPokemonInfo, setPokemonGender, pokemonGender}}>
       <div id="pokedexWrapper">
         <form onSubmit={handleSubmit}>
           <input type="text" id="SearchBox" placeholder="Type a pokemon name or id" ref={pokemonNameRef}/>
--- src/components/GenderButtons.jsx
+++ src/components/GenderButtons.jsx
@@ -1,11 +1,44 @@
+import React, {useContext} from "react";
+import { PokemonContext } from "../PokemonContext";
 
 function GenderButtons(){
+    
+    const {pokemonGender} = useContext(PokemonContext);
+
     return(
         <div className="center-left-container">
-            <div className="white-squares-container">
-              <div className="white-square"></div>
-              <div className="white-square"></div>
-            </div>
+            {(() => {
+                switch (pokemonGender) {
+                    case 0:
+                        return (
+                        <div className="white-squares-container">
+                            <div className="white-square"><i className="fa-solid fa-mars"></i></div>
+                            <div className="white-square"></div>
+                          </div>
+                        )
+                    case 8:
+                        return (
+                        <div className="white-squares-container">
+                            <div className="white-square"></div>
+                            <div className="white-square"><i className="fa-solid fa-venus"></i></div>
+                        </div>
+                        )
+                    case -1:
+                        return (
+                        <div className="white-squares-container">
+                            <div className="white-square"></div>
+                            <div className="white-square"></div>
+                        </div>
+                        )
+                    default:
+                        return (
+                        <div className="white-squares-container">
+                            <div className="white-square"><i className="fa-solid fa-mars"></i></div>
+                            <div className="white-square"><i className="fa-solid fa-venus"></i></div>
+                        </div>
+                        )
+                }
+            })()}
         </div>
     )
 }
